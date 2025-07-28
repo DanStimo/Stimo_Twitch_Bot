63caddc18f264436d8bb9eba1bef4bd916f0da72
DanStimo <stimodj@outlook.com>
2025-07-28 21:58:10 +0100

    Update stimobot.py

--- stimobot.py
+++ stimobot.py
@@ -316,7 +316,7 @@ class Bot(commands.Bot):
         )
 
     async def event_ready(self):
-        print(f"Logged in as | {self.nick}")
+        print(f"Logged in as | {self._connection._user.name}")
         await update_club_mapping_from_recent_matches(167054)
 
         # Start Discord client just long enough to send the message
