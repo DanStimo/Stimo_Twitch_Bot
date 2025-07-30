7390287147633cac11227b1178c36d1f7d58063e
DanStimo <stimodj@outlook.com>
2025-07-30 19:59:36 +0100

    Update stimobot.py

--- stimobot.py
+++ stimobot.py
@@ -114,6 +114,8 @@ class Bot(commands.Bot):
             bot_id=BOT_ID
         )
 
+        self.load_modules(__name__)
+
     async def event_ready(self):
         print(f"✅ Bot is online as: {self.nick if hasattr(self, 'nick') else self.user.name}")
 
